176d07dc6869ffcac729e6f2db43623bfb137ef2
DawidForst <111081668+DawidForst@users.noreply.github.com>
2023-06-24 14:42:22 +0200

    hw04

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,35 @@
-// const fs = require('fs/promises')
+const Contact = require("./contact.model");
 
-const listContacts = async () => {}
+const listContacts = async (filter = {}, page = 1, limit = 20) => {
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find(filter).skip(skip).limit(limit).exec();
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  return await Contact.findById(contactId);
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  return await Contact.findByIdAndRemove(contactId);
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  return await Contact.create(body);
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  return await Contact.findByIdAndUpdate(contactId, body, { new: true });
+};
+
+const updateContactFavorite = async (contactId, favorite) => {
+  const contact = await Contact.findById(contactId);
+  if (!contact) {
+    return null;
+  }
+  contact.favorite = favorite;
+  return await contact.save();
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +37,5 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+  updateContactFavorite,
+};
